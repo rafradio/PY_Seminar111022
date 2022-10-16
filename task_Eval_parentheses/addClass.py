7f208a3bf023b75dd1393259b17cd37720313f55
Rafail Abdyushev <rafaelab@mail.ru>
2022-10-16 17:21:02 +0300

    Task eval parethesis modified

--- task_Eval_parentheses/addClass.py
+++ task_Eval_parentheses/addClass.py
@@ -5,7 +5,7 @@ class InitSets:
     def __init__(this):
         this.clear = lambda: os.system("CLS")
         this.clear()
-        this.string = "2**3*6 - 5**3 * 9 / 3**2 - 21"
+        this.string = "(10 * (5 -2))**2 + (5 + (8**2 - 30**2 / 30))"
         #this.string = "20-3-4"
         this.anlytic = []
         this.levelMath = {
@@ -15,12 +15,10 @@ class InitSets:
             4: "+",
             5: "-"
         }
-        this.stringDict = {}
         this.AnalyseString()
         print(this.string)
-        this.MathCalc()
+        this.IsOrNotParentheses()
         
-
     def AnalyseString(this):
         this.string = this.string.replace(" ", "")
         posInt = ""
@@ -44,11 +42,40 @@ class InitSets:
                 posInt = ""
             prev = i
 
-        this.anlytic.append(posInt)
+        if posInt != "": this.anlytic.append(posInt)
         this.string = list(this.anlytic)
         this.whole_string = list(this.anlytic)
 
+    def IsOrNotParentheses(this):
+        while len(this.string) > 1 :
+            if "(" in this.whole_string:
+                this.FindParentheses(0)
+                print(this.parethBegin, this.parethEnd)
+                this.string = this.whole_string[this.parethBegin + 1: this.parethEnd]
+                this.MathCalc()
+                this.string = this.whole_string[: this.parethBegin] + this.string + this.whole_string[this.parethEnd + 1:]
+                this.whole_string = this.string
+                print(this.string)
+            else:
+                this.string = this.whole_string
+                this.MathCalc()
+
+    def FindParentheses(this, begin):
+        for i in range(begin, len(this.whole_string)):
+            if this.whole_string[i] == "(":
+                this.parethBegin = i
+                if "(" in this.whole_string[this.parethBegin + 1:]: this.FindParentheses(this.parethBegin + 1)
+                else:
+                    for j in range(this.parethBegin + 1, len(this.whole_string)):
+                        if this.whole_string[j] == ")": 
+                            this.parethEnd = j
+                            break
+                              
+                    break 
+                return 
+
     def MathCalc(this):
+
         while len(this.string) > 1 :
             for ii in this.levelMath:
                 if this.levelMath[ii] in this.string:
